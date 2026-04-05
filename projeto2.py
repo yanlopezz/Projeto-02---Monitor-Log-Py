cebb9ea0ee33bdff46b2f1be5cd036aa60b5d147
yanlopezz <yanlopessilva@gmail.com>
2026-04-05 17:48:22 -0300

    Monitor Log Py

--- projeto2.py
+++ projeto2.py
@@ -2,7 +2,7 @@ import random
 import datetime
 
 def menu():
-    nome_arq = 'log txt'
+    nome_arq = 'log.txt'
     while True:
         print('-' * 15)
         print('Monitor LogPy')
@@ -32,7 +32,7 @@ def menu():
             print('Opção errada')
 
 def gerarArquivo(nome_arq, qtd):
-    with open(nome_arq, 'w', enconding= 'UTF-8') as arq:
+    with open(nome_arq, 'w', enconding= 'utf-8') as arq:
          for i in range(qtd):
             arq.write(montarlog(i) + '\n')
          print('Logs gerados')
@@ -41,21 +41,21 @@ def montarlog(i):
     data = gerarDataHora(i)
     ip = gerarIp(i)
     recurso = gerarRecurso (i)
-    metodo = gerarMetodo(i)
-    status = gerarStatus (i)
-    tempo = gerarTempo (i)
+    metodo = gerarMetodo(recurso)
+    status = gerarStatus (i, recurso)
+    tempo = gerarTempo (i, status)
     agente = gerarAgente(i)
     return f'[{data}] {ip} - {metodo} - {status} - {recurso} - {tempo}ms - 500mb - HTTP/1.1 - {agente} - /home'
 
 def gerarDataHora(i):
     base = datetime.datetime(2026, 3, 30, 22,8,0)
     data = datetime.timedelta(seconds=i * random.randint(5,20))
-    return (base + data.stftime)('%d,%m/%Y %H:%M:%S')
+    return (base + data).strftime('%d/%m/%Y %H:%M:%S')
 
 def gerarIp(i):
     r = random.randint(1,6)
 
-    return f'{random.radint(10,255)}.{random.randint(1,255)}.{random.randint(1,255)}.{random.randint(1,255)}'
+    return f'{random.randint(10,255)}.{random.randint(1,255)}.{random.randint(1,255)}.{random.randint(1,255)}'
 
 def gerarRecurso(i):
     r = random.randint(1,4)
@@ -79,10 +79,10 @@ def gerarRecurso(i):
         return '/home'
 
 
-def gerarMetodo(i):
+def gerarMetodo(recurso):
     r = random.randint(1,2)
 
-    if i >= 10 and i <= 20:
+    if recurso >= 10 and recurso <= 20:
         return 'POST'
     
     if r == 1:
@@ -91,7 +91,7 @@ def gerarMetodo(i):
         return 'POST'
 
 
-def gerarStatus(i):
+def gerarStatus(i,recurso):
     r = random.randint(1,4)
 
     if i >= 10 and i <= 20:
@@ -113,5 +113,51 @@ def gerarStatus(i):
         return '500'
     
     
-def gerarTempo (i):
+def gerarTempo (i,status):
+    r = random.randint(1,3)
+
+    if i >= 50 and i <= 60:
+        return random.randint(800,1200)
     
+    if i >= 70 and i <= 75:
+        return 150 + (i - 70) * 150
+    
+
+    if r == 1:
+        return random.randint(50,199)
+    elif r == 2:
+        return random.randint(200,799)
+    else:
+        return random.randint(800,1200)
+    
+def gerarAgente(i):
+    r = random.randint(1,4)
+
+    if i >= 80 and i <= 85:
+        return 'Googlebot'
+    
+    if i >= 90 and i <= 95:
+        return 'Crawler'
+    
+    if r == 1:
+        return 'Google'
+    elif r == 2:
+        return 'Firefox'
+    elif r == 3:
+        return 'Edge'
+    else:
+        return 'Safari'
+    
+def classificarStatus(status):
+
+    if status == '200':
+        return 'Sucesso'
+    elif status == '403':
+        return 'Acesso negado'
+    elif status == '404':
+        return 'Erro leve'
+    else:
+        return 'Erro crítico'
+      
+def analisarLog(nome_arq):
+    pass
